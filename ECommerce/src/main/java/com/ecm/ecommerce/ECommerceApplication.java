96944534aaee7a463bf75b71795ca40c7d541bee
Viktor <benedekvik@gmail.com>
2024-01-18 19:49:53 +0100

    modified dependencies

--- ECommerce/src/main/java/com/ecm/ecommerce/ECommerceApplication.java
+++ ECommerce/src/main/java/com/ecm/ecommerce/ECommerceApplication.java
@@ -1,13 +1,47 @@
 package com.ecm.ecommerce;
 
+import com.ecm.ecommerce.Model.Role;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ECommerceApplication {
+@SecurityScheme(name = "E-Commerce Application", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
+public class ECommerceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ECommerceApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+    @Override
+    public void run(String... args) throws Exception {
+//        try {
+//            Role adminRole = new Role();
+//            adminRole.setRoleId(AppConstants.ADMIN_ID);
+//            adminRole.setRoleName("ADMIN");
+//
+//            Role userRole = new Role();
+//            userRole.setRoleId(AppConstants.USER_ID);
+//            userRole.setRoleName("USER");
+//
+//            List<Role> roles = List.of(adminRole, userRole);
+//
+//            List<Role> savedRoles = roleRepo.saveAll(roles);
+//
+//            savedRoles.forEach(System.out::println);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+    }
 }
